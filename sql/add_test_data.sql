d72b11dd1e5462e98c880163f6b8e0736572766d
otsepp <o.tuomas.seppanen@gmail.com>
2017-03-23 23:34:53 +0200

    documentaatio

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,21 @@
 -- Lisää INSERT INTO lauseet tähän tiedostoon
+
+INSERT INTO Laitos (nimi) VALUES ('Tietojenkäsittelytieteen laitos');
+
+INSERT INTO Oppilas (laitosID, nimi) VALUES (1, 'Pekka');
+INSERT INTO Opettaja (laitosID, nimi) VALUES (1, 'Arto');
+INSERT INTO Vastuuhenkilö (laitosID, nimi) VALUES (1, 'Matti');
+
+INSERT INTO Kurssi (laitosId, opettajaId, nimi) VALUES (1, 1, 'Tietokantojen perusteet');
+INSERT INTO Ilmoittautuminen (kurssiID, oppilasID) VALUES (1, 1);
+
+
+INSERT INTO Kysely (kurssiID, oppilasID, kommentti) VALUES (1, 1, 'Tylsä');
+
+INSERT INTO Kysymys (sisältö) VALUES ('Oliko kurssi vaikea?');
+INSERT INTO Kysymys (laitosID, sisältö) VALUES ('1, Opettiko kurssi tkt:sta?');
+INSERT INTO Kysymys (kurssiID, sisältö) VALUES ('1, Oliko ryhmätyö haastava?');
+
+INSERT INTO Kyselynkysymys(kyselyID, kysymysID, arvosana) VALUES (1, 1, 4);
+INSERT INTO Kyselynkysymys(kyselyID, kysymysID, arvosana) VALUES (1, 2, 3);
+INSERT INTO Kyselynkysymys(kyselyID, kysymysID, arvosana) VALUES (1, 3, 2);
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,66 @@
 -- Lisää CREATE TABLE lauseet tähän tiedostoon
+
+CREATE TABLE Laitos (
+	id SERIAL PRIMARY KEY,
+	nimi varchar(50) NOT NULL
+);
+
+
+CREATE TABLE Oppilas (
+	id SERIAL PRIMARY KEY,
+	laitosID INTEGER REFERENCES Laitos(id),
+	nimi varchar(50) NOT NULL
+);
+
+CREATE TABLE Opettaja (
+	id SERIAL PRIMARY KEY,
+	laitosID INTEGER REFERENCES Laitos(id),
+	nimi varchar(50) NOT NULL
+);
+
+CREATE TABLE Vastuuhenkilö (
+	id SERIAL PRIMARY KEY,
+	laitosID INTEGER REFERENCES Laitos(id),
+	nimi varchar(50) NOT NULL
+);
+
+
+CREATE TABLE Kurssi (
+	id SERIAL PRIMARY KEY,
+	laitosID INTEGER REFERENCES Laitos(id),
+	opettajaID INTEGER REFERENCES Opettaja(id),
+	nimi varchar(50) NOT NULL
+);
+
+CREATE TABLE Ilmoittautuminen (
+	kurssiID INTEGER REFERENCES Kurssi(id),
+	oppilasID INTEGER REFERENCES Oppilas(id)
+);
+
+
+CREATE TABLE Kysely (
+	id SERIAL PRIMARY KEY,
+	kurssiID INTEGER REFERENCES Kurssi(id),
+	oppilasID INTEGER REFERENCES Oppilas(id),
+	kommentti varchar(500) NOT NULL
+);
+
+CREATE TABLE Kysymys (
+	id SERIAL PRIMARY KEY,
+	laitosID INTEGER REFERENCES Laitos(id),
+	kurssiID INTEGER REFERENCES Kurssi(id),
+	sisaltö varchar(200) NOT NULL
+);
+
+CREATE TABLE Kyselynkysymys (
+	kyselyID INTEGER REFERENCES Kysely(id),
+	kysymysID INTEGER REFERENCES Kysymys(id), 
+	arvosana INTEGER NOT NULL
+);
+
+
+
+
+
+
+
