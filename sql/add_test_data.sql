d9ddc30582ba59659054da18a66d1068b819d723
Seppänen Tuomas O <tuomasse@da5-cs-b221-07.pc.helsinki.fi>
2017-04-11 16:00:42 +0300

    sql

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -3,17 +3,21 @@ INSERT INTO Laitos (nimi) VALUES ('Historian laitos');
 
 INSERT INTO Oppilas (laitos_id, nimi, salasana) VALUES (1, 'Pekka', 'ssana');
 INSERT INTO Oppilas (laitos_id, nimi, salasana) VALUES (1, 'Mauri', 'ssana');
+
 INSERT INTO Opettaja (laitos_id, nimi, salasana) VALUES (1, 'Arto', 'ssana');
 INSERT INTO Opettaja (laitos_id, nimi, salasana) VALUES (1, 'Jaakko', 'ssana');
 INSERT INTO Opettaja (laitos_id, nimi, salasana) VALUES (2, 'Juuso', 'ssana');
 INSERT INTO Opettaja (laitos_id, nimi, salasana) VALUES (2, 'Kari','ssana');
+
 INSERT INTO Vastuuhenkilo (laitos_id, nimi, salasana) VALUES (1, 'Matti', 'ssana');
 INSERT INTO Vastuuhenkilo (laitos_id, nimi, salasana) VALUES (2, 'Tuomas', 'ssana');
 
 INSERT INTO Kurssi (laitos_id, opettaja_id, nimi, kysely_kaynnissa) VALUES (1, 1, 'Tietokantojen perusteet', 1);
 INSERT INTO Kurssi (laitos_id, opettaja_id, nimi, kysely_kaynnissa) VALUES (1, 2, 'Tietokoneen toiminta', 1);
 INSERT INTO Kurssi (laitos_id, opettaja_id, nimi) VALUES (1, 1, 'Webdevving (online course)');
-INSERT INTO Kurssi (laitos_id, opettaja_id, nimi) VALUES (1, 4, 'Game Dev Course');
+INSERT INTO Kurssi (laitos_id, opettaja_id, nimi) VALUES (1, 2, 'Game Dev Course');
+INSERT INTO Kurssi (laitos_id, opettaja_id, nimi) VALUES (2, 3, 'Barokkiajan taide & muoti');
+INSERT INTO Kurssi (laitos_id, opettaja_id, nimi) VALUES (2, 4, 'Toinen maailmansota');
 
 INSERT INTO Ilmoittautuminen (kurssi_id, oppilas_id) VALUES (1, 1);
 INSERT INTO Ilmoittautuminen (kurssi_id, oppilas_id) VALUES (2, 2);
